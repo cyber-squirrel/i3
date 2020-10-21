e7876436cff67238e9d9fa2b59a8967162184e16
Cyber-Squirrel <28680422+cyber-squirrel@users.noreply.github.com>
2020-10-21 23:32:03 +1100

    Update i3-update.sh

--- i3-update.sh
+++ i3-update.sh
@@ -1,5 +1,7 @@
 # /usr/lib/apt/apt-helper download-file https://debian.sur5r.net/i3/pool/main/s/sur5r-keyring/sur5r-keyring_2020.02.03_all.deb keyring.deb SHA256:c5dd35231930e3c8d6a9d9539c846023fe1a08e4b073ef0d2833acd815d80d48
 # dpkg -i ./keyring.deb
-echo "deb https://debian.sur5r.net/i3/ $(grep '^DISTRIB_CODENAME=' /etc/lsb-release | cut -f2 -d=) universe" >> /etc/apt/sources.list.d/sur5r-i3.list
+# echo "deb https://debian.sur5r.net/i3/ $(grep '^DISTRIB_CODENAME=' /etc/lsb-release | cut -f2 -d=) universe" >> /etc/apt/sources.list.d/sur5r-i3.list
 # apt update
 # apt install i3
+sudo apt install -y lightdm
+# sudo systemctl enable lightdm
